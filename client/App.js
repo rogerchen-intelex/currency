ef150f394b80fb9d8cd7a2a607fd9275ce1a3d66
Roger Chen <Roger.Chen@intelex.com>
2017-10-03 15:57:49 -0400

    First Commit

---
+++ client/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Rate from './Rate/Rate';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Rate></Rate>
+      </div>
+    );
+  }
+}
+
+export default App;
