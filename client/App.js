7c51ccc73f1094490696dce23b7c641e28b8da8b
Roger Chen <Roger.Chen@intelex.com>
2017-10-09 23:15:35 -0400

    Routes

--- client/App.js
+++ client/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Rate from './Rate/Rate';
-
+// import Rate from './Rate/Rate';
+import Header from './Header.js'
+import Routes from './Routes.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Rate></Rate>
+          <Header />
+          <Routes />
       </div>
     );
   }
