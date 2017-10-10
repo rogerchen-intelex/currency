052b75435b087cd9d14cf04f8b4b1203749634e1
Roger Chen <Roger.Chen@intelex.com>
2017-10-09 23:26:37 -0400

    Routes

---
+++ client/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+      return (
+        <div>
+          I can tell all about it.
+        </div>
+      );
+    }
+  }
+
+  export default About;
